5e3bd0de528b01ba1a863d0d9045638b992f7a4b
toan2324 <163374397+toan2324@users.noreply.github.com>
2026-04-14 13:53:09 +0700

    Add logic for tinhTongChan

---
+++ MainTest.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+    @Test
+    void testCoSoChan() {
+        assertEquals(6, Main.tinhTongChan(4));
+    }
+
+    @Test
+    void testKhongCoSoChan() {
+        assertEquals(0, Main.tinhTongChan(1));
+    }
+}
